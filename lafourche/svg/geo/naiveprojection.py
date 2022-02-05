b83b13d095c62f64907fcd08be5a41d3c6fbebbd
Marguerite Vial <marguerite.vial@nc-ass-vip.sdv.fr>
2022-02-05 23:13:45 +0100

    fix unittests for naiveprojection

--- lafourche/svg/geo/naiveprojection.py
+++ lafourche/svg/geo/naiveprojection.py
@@ -1,4 +1,5 @@
 import logging
+import math
 
 from .projection import Projection
 
@@ -44,6 +45,7 @@ class NaiveProjection(Projection):
         return NaiveProjection(canvas_size, bottom_left_geopoint, top_right_geopoint)
 
     def project(self, longitude: float, latitude: float) -> (int, int):
+        self.__validate(longitude, latitude)
         abscissa = self.__get_abscissa(longitude)
         ordinate = self.__get_ordinate(latitude)
         self.logger.debug("Converting (%s; %s) into (%s; %s)", longitude, latitude, abscissa, ordinate)
@@ -53,17 +55,20 @@ class NaiveProjection(Projection):
         return self.__width, self.__height
 
     def __validate(self, longitude: float, latitude: float):
-        # TODO(mv): implement
-        pass
+        if longitude < self.__bottom_left_lon or longitude > self.__top_right_lon \
+                or latitude < self.__bottom_left_lat or latitude > self.__top_right_lat:
+            raise ValueError("Coordinate Out of Range")
 
     def __get_abscissa(self, longitude: float) -> int:
-        return (longitude - self.__bottom_left_lon) / (self.__top_right_lon - self.__bottom_left_lon) * self.__width
+        center_lon = (self.__top_right_lon + self.__bottom_left_lon) / 2
+        return round((longitude - center_lon) / (self.__top_right_lon - self.__bottom_left_lon) * self.__width)
 
     def __get_ordinate(self, latitude: float) -> int:
-        return (latitude - self.__bottom_left_lat) / (self.__top_right_lat - self.__bottom_left_lat) * self.__height
+        center_lat = (self.__top_right_lat + self.__bottom_left_lat) / 2
+        return round((latitude - center_lat) / (self.__top_right_lat - self.__bottom_left_lat) * self.__height)
 
     @staticmethod
     def get_canvas_height(canvas_width: int, bottom_left_geopoint: (float, float),
                           top_right_geopoint: (float, float)) -> int:
-        height_width_ratio = (self.__top_right_lat - self.__bottom_left_lat) / (self.__top_right_lon - self.__bottom_left_lon)
-        return canvas_width * height_width_ratio
+        height_width_ratio = (top_right_geopoint[1] - bottom_left_geopoint[1]) / (top_right_geopoint[0] - bottom_left_geopoint[0])
+        return math.ceil(canvas_width * height_width_ratio)
--- tests/svg/geo/test_naiveprojection.py
+++ tests/svg/geo/test_naiveprojection.py
@@ -7,17 +7,13 @@ class TestNaiveProjection(unittest.TestCase):
 
     def test_naive_projection_canvas_height(self):
         height = NaiveProjection.get_canvas_height(256, (2.32544, 48.88745), (2.32573, 48.88758))
-        self.assertEqual(height, None)  # TODO(mv): implement
+        self.assertEqual(height, 115)
 
     def test_naive_projection(self):
         projection = NaiveProjection.create(256, (2.32544, 48.88745), (2.32573, 48.88758))
-
-        # TODO(mv): update test values
-        self.assertEqual(projection.canvas_size(), (256, None))
-
-        # self.assertEqual(projection.project(2.32544, 48.88745), (-128, -128))
-        # self.assertEqual(projection.project(2.32544, 48.88758), (-128, 128))
-        # self.assertEqual(projection.project(2.32573, 48.88745), (128, -128))
-        # self.assertEqual(projection.project(2.32573, 48.88758), (128, 128))
-        #
-        # self.assertEqual(projection.project(2.325585, 48.887515), (0, 0))
+        self.assertEqual(projection.canvas_size(), (256, 115))
+        self.assertEqual(projection.project(2.32544, 48.88745), (-128, -58))
+        self.assertEqual(projection.project(2.32544, 48.88758), (-128, 58))
+        self.assertEqual(projection.project(2.32573, 48.88745), (128, -58))
+        self.assertEqual(projection.project(2.32573, 48.88758), (128, 58))
+        self.assertEqual(projection.project(2.325585, 48.887515), (0, 0))
